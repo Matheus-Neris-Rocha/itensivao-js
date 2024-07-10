1a1843235965cea8de9ec06bdb60b657ca35718c
Matheu Neris <matheusneris_xa@outlook.com>
2024-07-09 21:43:05 -0300

    intensivão js

---
+++ js/main.js
@@ -0,0 +1,103 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+// Resolução
+// alert("Hello Word!")
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+// Resolução
+// let number1 = Number(prompt("Digite o primeiro número"))
+// let number2 = Number(prompt("Digite o segundo número"))
+// let sum = number1 + number2
+// alert(`O resultado da soma é: ${sum}!`)
+
+/* 3. Crie um script que declare uma variável e verifique se o seu valor é um número.
+   Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+   💡 Para saber o tipo de dado você pode usar o operador `typeof`
+*/
+
+// Resolução
+// let checkNumber = 40
+// if(typeof checkNumber === "number") {
+//     alert("É um número")
+// } else { 
+//     alert("Não é um número")
+// }
+
+/* 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. 
+   Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+*/
+
+// Resolução
+// let checkString = "ABC"
+// if(typeof checkString === "string") {
+//     alert("É uma string")
+// } else {
+//     alert("Não é uma string")
+// }
+
+/* 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. 
+   Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+*/
+
+// Resolução
+// let checkBoolean = true
+// if(typeof checkBoolean === "boolean") {
+//     alert("É um booleano")
+// } else {
+//     alert("Não é um booleano")
+// }
+
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+// Resolução
+// let num1 = Number(prompt("Digite o primeiro número"))
+// let num2 = Number(prompt("Digite o segundo número"))
+// let sub = (num1 - num2)
+// alert(`O resultado da subtração é: ${sub}!`)
+
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+// Resolução
+// let num1 = Number(prompt("Digite o primeiro número"))
+// let num2 = Number(prompt("Digite o segundo número"))
+// let mult = (num1 * num2)
+// alert(`O resultado da multiplicação é: ${mult}`)
+
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+// Resolução
+// let num1 = Number(prompt("Digite o primeiro número"))
+// let num2 = Number(prompt("Digite o segundo número"))
+// let div = (num1 / num2)
+// alert(`O resultado da divisão é: ${div}`)
+
+/*
+9. Crie um script que declare uma variável e verifique se o seu valor é um número par. 
+
+Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem 
+"Não é um número par".
+*/
+
+// Resolução
+// let checkEvenNumber = Number(prompt("Digite um número"))
+// if(checkEvenNumber % 2 == 0) { 
+//     alert("É um número par")
+// } else {
+//     alert("Não é um número par")
+// }
+
+
+/*    
+10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar.
+Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem 
+"Não é um número ímpar".
+*/
+
+// Resolução
+// let checkOddNumber = Number(prompt("Digite um número"))
+// if(checkOddNumber % 2 != 0) {
+//     alert("É um número ímpar")
+// } else {
+//     alert("Não é um número ímpar")
+// }
